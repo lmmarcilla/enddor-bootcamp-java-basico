5ab490184f11006a5d29d98d37698b268c101bf1
Luis Miguel On MacBook <enddor@gmail.com>
2023-07-12 22:29:00 +0200

    Ejercicio 1

---
+++ src/com.example/ejercicio1.java
@@ -0,0 +1,30 @@
+package com.example;
+
+public class ejercicio1 {
+
+    public static void main(String[]args){
+
+        byte var1 = 2;
+        System.out.println("Variable 'byte': "+var1);
+        short var2 = 21;
+        System.out.println("Variable 'short': "+var2);
+        int var3 = 30;
+        System.out.println("Variable 'int': "+var3);
+        long var4 = 320;
+        System.out.println("Variable 'long': "+var4);
+
+        float var5 = 34.34F;
+        System.out.println("Variable 'float': "+var5);
+        double var6 = 2323.8787;
+        System.out.println("Variable 'double': "+var6);
+
+        boolean var7 = true;
+        System.out.println("Variable 'boolean': "+var7);
+
+        char var8 = 'g';
+        System.out.println("Variable 'char': "+var8);
+        String var9 = "Hola mundo";
+        System.out.println("Variable 'String': "+var9);
+
+    }
+}
